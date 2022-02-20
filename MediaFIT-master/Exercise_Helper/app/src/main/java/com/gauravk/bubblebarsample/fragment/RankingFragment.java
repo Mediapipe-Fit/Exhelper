adbb05608a45936f5accf758a5ce94e12b6b9f0c
Minjun Cha <62837074+whwhdnfl2@users.noreply.github.com>
2022-02-20 18:27:25 +0900

    [Updata] ranking fragment 구현

--- MediaFIT-master/Exercise_Helper/app/src/main/java/com/gauravk/bubblebarsample/fragment/RankingFragment.java
+++ MediaFIT-master/Exercise_Helper/app/src/main/java/com/gauravk/bubblebarsample/fragment/RankingFragment.java
@@ -1,10 +1,34 @@
 package com.gauravk.bubblebarsample.fragment;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import com.bumptech.glide.Glide;
 
+import com.gauravk.bubblebarsample.DB.Userdata.RetrofitAPI;
+import com.gauravk.bubblebarsample.DB.Userdata.user.dataall;
+import com.gauravk.bubblebarsample.DB.Userdata.user.post_response;
+import com.gauravk.bubblebarsample.DB.Userdata.user.user;
+import com.gauravk.bubblebarsample.DB.Userdata.RetrofitAPI;
+
+import java.util.HashMap;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.Body;
+import retrofit2.http.Field;
+import retrofit2.http.FieldMap;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+
+
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import com.gauravk.bubblebarsample.R;
@@ -15,14 +39,28 @@ public class RankingFragment extends Fragment {
         return fragment;
     }
 
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_ranking, container, false);
     }
 
+
     @Override
     public void onStart() {
         super.onStart();
+
+
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://www.exhelper.site/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        RetrofitAPI retrofitAPI = retrofit.create(RetrofitAPI.class);
+
     }
+
 }
